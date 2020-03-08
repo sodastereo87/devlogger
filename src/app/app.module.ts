ca41f006620d8b7de0b857526483b38a4a946de2
sodastereo <f_er_@hotmail.com>
2020-03-07 23:33:28 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { LogFormComponent } from "./components/log-form/log-form.component";
-import { LogsComponent } from "./components/logs/logs.component";
-import { LogService } from "./services/log.service";
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogService } from './services/log.service';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +16,11 @@ import { LogService } from "./services/log.service";
     LogFormComponent,
     LogsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   providers: [LogService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/services/log.service.spec.ts
+++ src/app/services/log.service.spec.ts
@@ -1,16 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { LogService } from './log.service';
 
 describe('LogService', () => {
-  let service: LogService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LogService);
+    TestBed.configureTestingModule({
+      providers: [LogService]
+    });
   });
 
-  it('should be created', () => {
+  it('should be created', inject([LogService], (service: LogService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
